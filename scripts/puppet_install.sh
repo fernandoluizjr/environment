13d754c6cd8af3c87a5f040a7a96128707bdae3e
Fernando Amaral <fernandoluizjr@gmail.com>
2015-11-20 21:59:54 -0200

    melhorias no script de instalacao do puppet

--- scripts/puppet_install.sh
+++ scripts/puppet_install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+#set -e -x
+
+# Garantir que a versao suporte config de proxy
+#if which puppet > /dev/null : them
+#  echo "Puppet is already installed"
+#  exit 0
+#fi
+
 apt-get update
-wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb -P /tmp
+wget -qO /tmp/puppetlabs-release-precise.deb https://apt.puppetlabs.com/puppetlabs-release-precise.deb
 dpkg -i /tmp/puppetlabs-release-precise.deb
+rm /tmp/puppetlabs-release-precise.deb
 apt-get update
+echo "Installing puppet"
 apt-get install --yes puppet
-puppet resource package puppet ensure=latest
+#puppet resource package puppet ensure=latest
+echo "Puppet installed!"
 
 exit 0
