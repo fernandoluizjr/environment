2ff088d99360b3477cfa32f2d920f84d676b7718
Fernando Amaral <fernandoluizjr@gmail.com>
2015-11-22 22:31:38 -0200

    ajuste no script de instalacao do puppet para remover variaveis deprecated e evitar warnings

--- scripts/puppet_install.sh
+++ scripts/puppet_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Remove puppet e outras gemas ruby instaladas pelo vagrant
+# Remove puppet e other gems instaled by vagrant
 if [ -d /opt/vagrant_ruby ]; then
-  echo "Removing directory /opt/vagrant_ruby from system and PATH"
+  echo "Removing directory /opt/vagrant_ruby from system and path"
   PATH=${PATH#/opt/vagrant_ruby/bin:}
   PATH=${PATH%:/opt/vagrant_ruby/bin}
   rm -rf /opt/vagrant_ruby
@@ -10,17 +10,22 @@ fi
 
 if which puppet >/dev/null ; then
   echo "Puppet is already installed"
-  exit 0
+else
+  apt-get update
+  wget -qO /tmp/puppetlabs-release-precise.deb \
+	https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+  dpkg -i /tmp/puppetlabs-release-precise.deb
+  rm /tmp/puppetlabs-release-precise.deb
+  apt-get update
+  echo "Installing puppet"
+  apt-get install --yes puppet
+  echo "Puppet installed!"
 fi
 
-apt-get update
-wget -qO /tmp/puppetlabs-release-precise.deb \
-	https://apt.puppetlabs.com/puppetlabs-release-precise.deb
-dpkg -i /tmp/puppetlabs-release-precise.deb
-rm /tmp/puppetlabs-release-precise.deb
-apt-get update
-echo "Installing puppet"
-apt-get install --yes puppet
-echo "Puppet installed!"
+# Remove some variables deprecated to avoid warnings
+grep -q "templatedir" /etc/puppet/puppet.conf
+if [ $? -eq 0 ]; then
+    sed -i '/templatedir/d' /etc/puppet/puppet.conf
+fi
 
 exit 0
