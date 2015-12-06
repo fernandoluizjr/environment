f2bf9a93e50e32374644ad1e3c76871215f3aef3
Fernando Amaral <fernandoluizjr@gmail.com>
2015-12-06 16:14:56 -0200

    ajustes na instalacao do java

--- development/modules/development/manifests/init.pp
+++ development/modules/development/manifests/init.pp
@@ -1,5 +1,11 @@
 class development {
 
+# Ensure that add-apt-repository binary is present
+# because some packages needs add PPA repositories
+  package { "python-software-properties":
+    ensure => installed,
+  }
+
   class { 'apt':
     update => {
       frequency => 'always',
--- development/modules/java/manifests/sdk.pp
+++ development/modules/java/manifests/sdk.pp
@@ -1,16 +1,16 @@
 class java::sdk {
 
-  exec {
-    command => "sudo apt-get purge openjdk*"
-  }
+#  exec { "purge-old-java":
+#    command => "apt-get purge openjdk*"
+#  }
 
   apt::ppa { 'ppa:webupd8team/java': }
 
-  exec { "accept-license":
-    command => "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 \
-				select true | sudo /usr/bin/debconf-set-selections",
-    path => "/usr/bin",
-  }
+#  exec { "accept-license":
+#    command => "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 \
+#				select true | /usr/bin/debconf-set-selections",
+#    path => "/usr/bin",
+#  }
 
   package { "oracle-java8-installer":
     ensure => installed,
