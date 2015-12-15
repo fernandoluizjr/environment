3c144c98b7ce9bed3a0299f1139729f6d3e0ce26
Fernando Amaral <fernandoluizjr@gmail.com>
2015-12-14 22:49:20 -0200

    adicionando unless na aceitacao da licenca do java

--- development/modules/java/manifests/sdk.pp
+++ development/modules/java/manifests/sdk.pp
@@ -8,6 +8,11 @@ class java::sdk {
     command => "/bin/echo oracle-java8-installer \
 				shared/accepted-oracle-license-v1-1 \
 				select true | /usr/bin/debconf-set-selections",
+    unless => "/usr/bin/debconf-get-selections \
+				| /bin/grep oracle-java8-installer \
+				| /bin/grep accepted-oracle-license-v1-1 \
+				| /bin/grep select \
+				| /bin/grep true",
   }
 
   package { "oracle-java8-installer":
