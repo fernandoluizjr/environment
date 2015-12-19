97b44af14bdb310df48c33925a9070160ab83041
Fernando Amaral <fernandoluizjr@gmail.com>
2015-12-19 14:43:12 -0200

    evolucao no mecanismo de configuracao do git para iterar sobre cada item do hash de parametros

--- development/modules/development/manifests/params.pp
+++ development/modules/development/manifests/params.pp
@@ -1,8 +1,8 @@
 class development::params {
 
   $git_config_global = {
-    user_name => "Fernando Amaral",
-    user_email => "fernandoluizjr@gmail.com",
-    core_editor => "vim",
+    "user.name" => "Fernando Amaral",
+    "user.email" => "fernandoluizjr@gmail.com",
+    "core.editor" => "vim",
   }
 }
--- development/modules/git/manifests/config.pp
+++ development/modules/git/manifests/config.pp
@@ -1,23 +1,11 @@
 define git::config($global) {
 
-  exec { "global-user-name":
-    unless => "git config --global --get user.name | /bin/grep \"${global[user_name]}\"",
-    command => "git config --global user.name \"${global[user_name]}\"",
-    environment => "HOME=/home/vagrant",
-    path => "/usr/bin/",
-  }
-
-  exec { "global-user-email":
-    unless => "git config --global --get user.email | /bin/grep \"${global[user_email]}\"",
-    command => "git config --global user.email \"${global[user_email]}\"",
-    environment => "HOME=/home/vagrant",
-    path => "/usr/bin/",
-  }
-
-  exec { "global-core-editor":
-    unless => "git config --global --get core.editor | /bin/grep \"${global[core_editor]}\"",
-    command => "git config --global core.editor \"${global[core_editor]}\"",
-    environment => "HOME=/home/vagrant",
-    path => "/usr/bin/",
+  $global.each |$key, $val| {
+    exec { "git-config-global-$key":
+      unless => "git config --global --get $key | /bin/grep \"$val\"",
+      command => "git config --global $key \"$val\"",
+      environment => "HOME=/home/vagrant",
+      path => "/usr/bin/",
+    }
   }
 }
