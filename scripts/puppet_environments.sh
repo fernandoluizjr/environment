c3df96e64ce1b268c6162da9f78f7a302474c213
Fernando Amaral <fernandoluizjr@gmail.com>
2015-12-22 23:09:28 -0200

    configurando path global de modulos compartilhados

--- scripts/puppet_environments.sh
+++ scripts/puppet_environments.sh
@@ -3,6 +3,7 @@
 directory_environments=$1
 active_node_environment=$2
 manifest_file=$directory_environments/$active_node_environment/manifests/$3
+base_module_path=\$confdir/modules:$directory_environments/modules
 
 # Enable directory environments
 grep -q "environmentpath=" /etc/puppet/puppet.conf
@@ -18,6 +19,14 @@ if [ $? -eq 0 ]; then
     sed -i '/environment=/c\environment='"$active_node_environment" /etc/puppet/puppet.conf
 else
     sed -i 's|\[main\]|&\nenvironment='"$active_node_environment"'|' /etc/puppet/puppet.conf
+fi
+
+# Setting the basemodulepath to global modules for all environments
+grep -q "basemodulepath=" /etc/puppet/puppet.conf
+if [ $? -eq 0 ]; then
+    sed -i '/basemodulepath=/c\basemodulepath='"$base_module_path" /etc/puppet/puppet.conf
+else
+    sed -i 's|\[main\]|&\nbasemodulepath='"$base_module_path"'|' /etc/puppet/puppet.conf
 fi
 
 # Apply puppet manifest
