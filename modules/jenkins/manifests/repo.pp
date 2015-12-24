12a00ccc8acaed5eb8c325ca296f46ec8c5d0ac0
Fernando Amaral <fernandoluizjr@gmail.com>
2015-12-24 18:24:20 -0200

    correcao na url para o repositorio do jenkins

--- modules/jenkins/manifests/repo.pp
+++ modules/jenkins/manifests/repo.pp
@@ -5,7 +5,7 @@ class jenkins::repo {
     repos => '',
     key => {
       id => '150FDE3F7787E7D11EF4E12A9B7D32F2D50582E6',
-      source => 'https://jenkins-ci.org/debian/jenkins-ci.org.key',
+      source => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
     },
   }
 }
