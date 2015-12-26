2d584283167066fffa7f3733c9fc8853efe6e0f0
Fernando Amaral <fernandoluizjr@gmail.com>
2015-12-26 14:19:17 -0200

    criando define para instalacao de plugins do jenkins

---
+++ modules/jenkins/manifests/plugins.pp
@@ -0,0 +1,40 @@
+define jenkins::plugins($plugins) {
+  $plugin_dir = '/var/lib/jenkins/plugins'
+  $plugins_host = 'https://updates.jenkins-ci.org'
+  $base_url = "${plugins_host}/latest/"
+
+  file { $plugin_dir:
+    ensure => 'directory',
+    owner => 'jenkins',
+    group => 'jenkins',
+    mode => '0644',
+  }
+
+  $plugins.each |$plugin_name| {
+    $download_url = "${base_url}${plugin_name}.hpi"
+    $plugin_ext = regsubst($download_url, '^.*\.(hpi|jpi)$', '\1')
+    $plugin = "${plugin_name}.${plugin_ext}"
+
+    exec { "$plugin":
+      command => "/usr/bin/wget -qO ${plugin_dir}/${plugin} $download_url",
+      notify => Service['jenkins'],
+      require => File["$plugin_dir"],
+    }
+
+    file { "${plugin_dir}/${plugin}":
+      ensure => 'file',
+      owner => 'jenkins',
+      group => 'jenkins',
+      mode => '0644',
+      require => Exec["$plugin"],
+    }
+
+    file { "${plugin_dir}/${plugin}.pinned":
+      ensure => 'file',
+      owner => 'jenkins',
+      group => 'jenkins',
+      mode => '0644',
+      require => File["${plugin_dir}/${plugin}"],
+    }
+  }
+}
