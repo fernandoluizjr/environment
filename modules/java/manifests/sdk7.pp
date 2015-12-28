74b4feda30180acb9bea887770fde4b5fedd50ab
Fernando Amaral <fernandoluizjr@gmail.com>
2015-12-27 22:15:26 -0200

    separando a classe sdk nas versoes 7 e 8 do java

---
+++ modules/java/manifests/sdk7.pp
@@ -0,0 +1,18 @@
+class java::sdk {
+  require java::repo
+
+  exec { 'accept-java7-license':
+    command => "/bin/echo oracle-java7-installer \
+			shared/accepted-oracle-license-v1-1 \
+			select true | /usr/bin/debconf-set-selections",
+    unless => "/usr/bin/dpkg -l | grep oracle-java7-installer",
+  }
+
+  package { 'oracle-java7-installer':
+    ensure => installed,
+    require => [
+                Exec['accept-java7-license'],
+                Class['apt::update'],
+               ],
+  }
+}
