39ea6ab04b53cd40886b174831490b4303507824
Fernando Amaral <fernandoluizjr@gmail.com>
2015-12-27 23:55:55 -0200

    adicionando recursos de configuracao do maven e do job para o jenkins

--- loja_virtual/modules/loja_virtual/manifests/ci.pp
+++ loja_virtual/modules/loja_virtual/manifests/ci.pp
@@ -17,4 +17,39 @@ class loja_virtual::ci {
   jenkins::plugins { 'jenkins-plugins':
     plugins => $loja_virtual::params::jenkins_plugins,
   }
+
+  file { '/var/lib/jenkins/hudson.tasks.Maven.xml':
+    mode => '0644',
+    owner => 'jenkins',
+    group => 'jenkins',
+    source => 'puppet:///modules/loja_virtual/hudson.tasks.Maven.xml',
+    require => Package['jenkins'],
+    notify => Service['jenkins'],
+  }
+
+  $job_structure = [
+    '/var/lib/jenkins/jobs/',
+    '/var/lib/jenkins/jobs/loja-virtual-devops'
+  ]
+
+  $project_repository_url = 'https://github.com/fernandoluizjr/loja-virtual-devops'
+  $repository_poll_interval = '* * * * *'
+  $maven_goal = 'install'
+  $archive_artifacts = 'combined/target/*.war'
+
+  file { $job_structure:
+    ensure => 'directory',
+    owner => 'jenkins',
+    group => 'jenkins',
+    require => Package['jenkins'],
+  }
+
+  file { "${job_structure[1]}/config.xml":
+    mode => '0644',
+    owner => 'jenkins',
+    group => 'jenkins',
+    content => template('loja_virtual/config.xml'),
+    require => File[$job_structure],
+    notify => Service['jenkins'],
+  }
 }
