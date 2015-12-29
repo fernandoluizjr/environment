3741b18e79210df2f1e8dc72e7308d70a30b0ade
Fernando Amaral <fernandoluizjr@gmail.com>
2015-12-29 02:46:37 -0200

    chamando a criacao do repositorio no provisionamento da maquina CI

--- loja_virtual/modules/loja_virtual/manifests/ci.pp
+++ loja_virtual/modules/loja_virtual/manifests/ci.pp
@@ -6,21 +6,26 @@ class loja_virtual::ci {
   include jenkins::jenkins
   include loja_virtual::params
 
+# Garante que o maven seja instalado antes do sdk e este antes do jenkins
   Class['maven::maven'] -> Class['java::sdk7']
   Class['java::sdk7'] -> Class['jenkins::jenkins']
 
+# Configura o git
   git::config { 'git-config':
     global => $loja_virtual::params::git_config_global,
   }
 
+# Configura o jenkins
   jenkins::config { 'jenkins-config':
     value => $loja_virtual::params::jenkins_config,
   }
 
+# Instala os plugins do jenkins
   jenkins::plugins { 'jenkins-plugins':
     plugins => $loja_virtual::params::jenkins_plugins,
   }
 
+# Atrela a instalacao do maven no jenkins
   file { '/var/lib/jenkins/hudson.tasks.Maven.xml':
     mode => '0644',
     owner => 'jenkins',
@@ -30,6 +35,7 @@ class loja_virtual::ci {
     notify => Service['jenkins'],
   }
 
+# Cria e configura o job para o build da loja_virtual
   $job_structure = [
     '/var/lib/jenkins/jobs/',
     '/var/lib/jenkins/jobs/loja-virtual-devops'
@@ -54,5 +60,14 @@ class loja_virtual::ci {
     content => template('loja_virtual/config.xml'),
     require => File[$job_structure],
     notify => Service['jenkins'],
+  }
+
+# Cria um repositorio de pacotes para os releases da loja_virtual
+  $repo_dir = '/var/lib/apt/repo'
+  $repo_name = 'devopspkgs'
+
+  class { 'loja_virtual::repo':
+    basedir => $repo_dir,
+    reponame => $repo_name,
   }
 }
