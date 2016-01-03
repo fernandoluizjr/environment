602083831680fd2d6bf36b1cc855796075fb9ff5
Fernando Amaral <fernandoluizjr@gmail.com>
2016-01-03 01:40:47 -0200

    removendo script de instalacao de modulos para usar o librarian-puppet

--- scripts/puppet_modules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-module_name=$1
-globalmodulepath=/opt/puppetlabs/puppet/modules
-
-# Install adicional modules from puppet forge
-puppet module install -i $globalmodulepath $module_name
